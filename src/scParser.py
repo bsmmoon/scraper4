561219c821073b6c1ee6a8d16a8c38bf29f631c3
bsmmoon <bsmmoon@gmail.com>
2016-01-02 00:22:28 +0800

    can get table

--- src/scParser.py
+++ src/scParser.py
@@ -1,3 +1,4 @@
+import re
 
 
 class scParser:
@@ -5,7 +6,25 @@ class scParser:
         print("scParser")
 
     def run(self, html):
-
-        
-
+        table = self.findTable(html)
         return ""
+
+    def findTable(self, html):
+        html = html.replace("\n", "")
+        html = self.find(html, "<table>", "</table>")
+        table = re.findall("<tr(.*?)</tr", html)
+        print(len(table))
+        for index in range(len(table)):
+            table[index] = re.findall("<td(.*?)</td", table[index])
+        print(len(table[10]))
+        return table
+
+    def find(self, text, front, back):
+        startIdx = text.find(front) + len(front)
+        offset = text[startIdx:].find(back)
+        endIdx = startIdx + offset
+        return text[startIdx:endIdx]
+
+    # def writeLog(self, text):
+    #     with open('log.html', 'w', encoding='UTF-8') as f:
+    #         f.write(text)
